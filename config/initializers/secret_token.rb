cfda53f467b82a08e4335753ea3305c724c68366
Boris German <boris@kontera.com>
2011-04-14 16:01:01 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'bec08a64f8a726321973f0935a5888d8165478993aa150b44c561b7aab0a99971c5a77fa07dca1a3fced8e060d3006f872871292104755882d158e5f18939f6a'
